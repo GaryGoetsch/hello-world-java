f6ad61169fb50cfb36e4b4b026487a05107bb28f
Gary Goetsch <garyg@astutesoft.co.za>
2011-11-30 14:09:07 +0200

    KAK CODE

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,5 +36,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("This line was added to test git push.");
+    //KAK CODE
   }
 }
